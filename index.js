24b922bd2cc968aceb88e6a7fef07bfdb1346df6
Sárándi <sarandi.balint@gmail.com>
2022-09-14 12:54:20 +0200

    csakegycommit

--- index.js
+++
@@ -1,8 +0,0 @@
-import { LOREM, NEV } from './kulonleges.js';
-import { veletlen } from './matek.js';
-
-console.log("Hello");
-console.log(veletlen(5,10));
-console.log(NEV + ' ' + LOREM);
-
-
---
+++ src/index.js
@@ -0,0 +1,16 @@
+//import { concat } from 'lodash';
+import _ from 'lodash';
+import { LOREM, NEV } from './szovegek/kulonleges.js';
+import { veletlen } from './matek.js';
+
+console.log("Hello");
+console.log(veletlen(5,10));
+console.log(NEV + ' ' + LOREM);
+
+
+let t1 = [ 1, 455, 6];
+let t2 = [454, 33, 1];
+let t3 = [2];
+
+let osszefuzott = _.concat(t1, t2, t3);
+console.log(osszefuzott);
